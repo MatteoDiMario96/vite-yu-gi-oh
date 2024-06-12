818c815ad6dc692c95dee126a3abea4c0b445196
MatteoDiMario96 <dm.matteo96@live.it>
2024-06-12 17:14:08 +0200

    Grazie alle props. Prima provato con gli store ma senza successo. Poi grazie alle props preso i dati degli api e trasferiti al components figlio. Creato props type array required true. Creazione di tutto il template. Che andra poi spostato in un unico componente.

--- src/components/AppMain/MainCardsList.vue
+++ src/components/AppMain/MainCardsList.vue
@@ -1,12 +1,19 @@
 <script>
 import MainCard from './MainCard.vue';
+import { store } from '../../store.js';
 export default {
     components:{
         MainCard,
     },
+    props:{
+        cards:{
+            type: Array,
+            required: true,
+        }
+    },
     data() {
         return {
-            
+            // store,
         }
     },
     methods:{
@@ -19,10 +26,57 @@ export default {
 </script>
 
 <template>
+    <div id="container">
+        <section>
+            <h2>
+                Found * cards
+            </h2>
+            <div id="cards">
+                <div class="card" v-for="(card,index) in cards" :key="card.id">
+                <div>
+                    <img :src="card.card_images[0].image_url" :alt="card.name">
+                </div>
+                <h3>
+                    {{ card.name }}
+                </h3>
+                <h4>
+                    {{ card.type }}
+                </h4>
+            </div>
+            </div>
+        </section>
+    </div>
     <!--MainCard-->
 </template>
 
 <style lang="scss" scoped>
+@use '../../styles/partials/variables' as *;
+
+div#container{
+    width: 100%;
+    padding: 1rem;
+    background-color: white;
+    section{
+        width: 100%;
+        h2{
+            background-color: black;
+            color: $text_color;
+        }
+            div#cards{
+                width: 100%;
+                display: flex;
+                flex-wrap: wrap;
+                div.card{
+                    width: calc((100% / 5) - .4rem);
+                    margin-right: .35rem;
+                    img{
+                        width: 100%;
+                    }
+                }
+            }
+        }
+    }
+    
 
 
 </style>
