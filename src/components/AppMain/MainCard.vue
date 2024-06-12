28d6b6046b9aadbc008e7c75cf937498985cc955
MatteoDiMario96 <dm.matteo96@live.it>
2024-06-12 17:46:53 +0200

    Spostato la singola card nel suo component. Creata la props singleCard. Incollato tutto quello che avveo creato nell'alto component. Fatto lo stesso con stile. Cambiato i parametri dinamici perchè cambiando props ho dovuto cambiare anche nome di di quelle variabili. Quindi da card.name a singleCard.name

--- src/components/AppMain/MainCard.vue
+++ src/components/AppMain/MainCard.vue
@@ -1,5 +1,11 @@
 <script>
 export default {
+    props:{
+        singleCard:{
+            type: Object,
+            required: true,
+        }
+    },
     data() {
         return {
             
@@ -15,10 +21,42 @@ export default {
 </script>
 
 <template>
-
+    <div class="card">
+        <div>
+            <img :src="singleCard.card_images[0].image_url" :alt="singleCard.name">
+        </div>
+        <h3>
+            {{ singleCard.name }}
+        </h3>
+        <h4>
+            {{ singleCard.type }}
+        </h4>
+    </div>
 </template>
 
 <style lang="scss" scoped>
-
+@use '../../styles/partials/variables' as *;
+div.card{
+    width: calc((100% / 5) - 1rem);
+    margin-right: 1rem;
+    background-color: $main_color;
+    margin-bottom: .8rem;
+    img{
+        width: 100%;
+        margin-bottom: .4rem;
+    }
+    h3{
+        color: $text_color;
+        text-align: center;
+        margin-bottom: .4rem;
+        font-size: .8rem;
+    }
+    h4{
+        font-weight: 100;
+        text-align: center;
+        font-size: .7rem;
+        margin-bottom: .4rem;
+    }
+}
 
 </style>
