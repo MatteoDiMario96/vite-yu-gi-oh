64464f452ac69c8fae8d47b53372b2dcc6fddc9e
MatteoDiMario96 <dm.matteo96@live.it>
2024-06-12 15:02:14 +0200

    Creazione scaffolding components e template.

---
+++ src/components/AppMain/MainCardsList.vue
@@ -0,0 +1,24 @@
+<script>
+export default {
+    data() {
+        return {
+            
+        }
+    },
+    methods:{
+
+    },
+    created(){
+
+    }
+}
+</script>
+
+<template>
+
+</template>
+
+<style lang="scss" scoped>
+
+
+</style>
