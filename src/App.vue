007c908f5283f5104fb43edfbf1abcc1765cd341
MatteoDiMario96 <dm.matteo96@live.it>
2024-06-12 14:43:39 +0200

    Creati i miei fogli di stile. Quello generale. Assegnato Roboto. Fatto Reset. Quelli parziali: Variabili. Mixine.

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script>
+// Import
+</script>
+
+<template>
+  
+</template>
+
+<style lang="scss">
+  @use './styles/generals.scss';
+  @use './styles/partials/variables' as *;
+  @use './styles/partials/mixins' as *;
+</style>
