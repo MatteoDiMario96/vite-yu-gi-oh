2642225c624d6143e13b1d7aa855e14d33ad026a
MatteoDiMario96 <dm.matteo96@live.it>
2024-06-12 15:42:58 +0200

    Creazione del component MainSelect.

---
+++ src/components/AppMain/MainSelect.vue
@@ -0,0 +1,29 @@
+<script>
+export default {
+    data() {
+        return {
+            
+        }
+    },
+    methods:{
+
+    },
+    created(){
+
+    }
+}
+</script>
+
+<template>
+    <select name="card-type" id="card-type">
+        <option value="nessuna">Scegli un tipo...</option>
+        <option value="macchina">Machine</option>
+        <option value="drago">Dragon</option>
+        <option value="psichico">Psychic</option>
+        <option value="zombie">Zombie</option>
+    </select>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
