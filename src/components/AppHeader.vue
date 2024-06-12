ca07b405b6f16e4a5a63499854b455fbfcb568a3
MatteoDiMario96 <dm.matteo96@live.it>
2024-06-12 15:25:17 +0200

    Popolato AppHeader. Creato header. Creato array oggetto logoPagina con url e title. Creato img e assegnato src e alt dinamico in base all'array oggetto. Creato un h1 titolo. Dato un certo stile.

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -2,7 +2,10 @@
 export default {
     data() {
         return {
-            
+            logoPagina:{
+                url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/1/11/Yu-Gi-Oh%21_%28Logo%29.jpg/1200px-Yu-Gi-Oh%21_%28Logo%29.jpg',
+                title: 'Yu-Gi-Oh, Gioco Di Carte',
+            }
         }
     },
     methods:{
@@ -16,13 +19,26 @@ export default {
 
 <template>
     <header>
+        <img :src="logoPagina.url" :alt="logoPagina.title">
         <h1>
-            Sono Header
+            Yu-Gi-Oh Api
         </h1>
     </header>
 </template>
 
 <style lang="scss" scoped>
-
+header{
+    display: flex;
+    align-items: center;
+    height: 90px;
+    padding: 1rem;
+    img{
+        height: 38%;
+    }
+    h1{
+        margin-left: 2rem;
+        font-size: 1.9rem;
+    }
+}
 
 </style>
