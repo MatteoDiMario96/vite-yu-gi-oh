b28cbe95369bb3330cad55a1c4ed9a143f534a19
MatteoDiMario96 <dm.matteo96@live.it>
2024-06-14 00:27:59 +0200

    Creata funzione che prende e comunica archetipo. Dico che l'indice  nell'array oggetti clickato storato optionsArchetype diventa il nuovo valore di pickedArchetype(archetypo  preso). Chiamata all'api tramite axios. Metto in store in archetypeCardsFounded il response.data. Cambiato correttamente nome alla funzione al click.

--- src/components/AppMain/MainSelect.vue
+++ src/components/AppMain/MainSelect.vue
@@ -26,10 +26,26 @@ export default {
         consoleLog(indice){
             console.log(`Mi dai l'${indice}`)
         },
-        comunicateArchetype(indice){
+        comunicateAndPickArchetype(indice){
             this.store.pickedArchetype = this.store.optionsArchetype[indice].archetype_name;
             console.log(this.store.pickedArchetype)
-        }
+            axios.get(`https://db.ygoprodeck.com/api/v7/cardinfo.php?archetype=${this.store.pickedArchetype}`)
+            .then((response) => {
+            
+                this.store.archetypeCardsFounded = response.data
+                console.log('archetpecards', this.store.archetypeCardsFounded)
+                // this.store.optionsArchetype = response.data
+                console.log(this.store.pickedArchetype);
+
+            })
+            .catch(function (error) {
+                // handle error
+                console.log(error);
+            })
+            .finally(function () {
+                // always executed
+            });
+        },
     },
     created(){
         this.getArchetype();
@@ -39,7 +55,7 @@ export default {
 
 <template>
     <select name="card-type" id="card-type">
-        <option value="option" v-for="(option,index) in store.optionsArchetype" :key="index" @click="comunicateArchetype(index)">
+        <option value="option" v-for="(option,index) in this.store.optionsArchetype" :key="index" @click="comunicateAndPickArchetype(index)">
             {{ option.archetype_name }}
         </option>
     </select>
