3888ed9a5d132a412c7d1cbd2a5a305325d0fe37
MatteoDiMario96 <dm.matteo96@live.it>
2024-06-13 18:53:06 +0200

    Creata logica nella mainSelect. Importato axios. Importato store. Fatto una chiamata GET axios per ricevere gli archetipi. storato i risultati. Creata funzione che mi assegni valore di una variabile al click di una mia option. Creato un select con una option e applicato un v-for e fatto iterare su optionsArchetype storato.

--- src/components/AppMain/MainSelect.vue
+++ src/components/AppMain/MainSelect.vue
@@ -1,26 +1,47 @@
 <script>
+import axios from 'axios';
+import { store } from '../../store';
 export default {
     data() {
         return {
-            
+            store,
         }
     },
     methods:{
-
+        getArchetype(){
+            axios.get('https://db.ygoprodeck.com/api/v7/archetypes.php')
+            .then((response) => {
+                // handle success
+                this.store.optionsArchetype = response.data
+                console.log(this.store.optionsArchetype);
+            })
+            .catch(function (error) {
+                // handle error
+                console.log(error);
+            })
+            .finally(function () {
+                // always executed
+            });
+        },
+        consoleLog(indice){
+            console.log(`Mi dai l'${indice}`)
+        },
+        comunicateArchetype(indice){
+            this.store.pickedArchetype = this.store.optionsArchetype[indice].archetype_name;
+            console.log(this.store.pickedArchetype)
+        }
     },
     created(){
-
+        this.getArchetype();
     }
 }
 </script>
 
 <template>
     <select name="card-type" id="card-type">
-        <option value="nessuna">Scegli un tipo...</option>
-        <option value="macchina">Machine</option>
-        <option value="drago">Dragon</option>
-        <option value="psichico">Psychic</option>
-        <option value="zombie">Zombie</option>
+        <option value="option" v-for="(option,index) in store.optionsArchetype" :key="index" @click="comunicateArchetype(index)">
+            {{ option.archetype_name }}
+        </option>
     </select>
 </template>
 
